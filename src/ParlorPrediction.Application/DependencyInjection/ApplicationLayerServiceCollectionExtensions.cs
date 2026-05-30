ac9b77948594aacd68db3b0034ac68afe6d3d87a
dagemov <97604053+dagemov@users.noreply.github.com>
2026-05-30 10:06:23 -0400

    feat(dough): add dough calculation service

--- src/ParlorPrediction.Application/DependencyInjection/ApplicationLayerServiceCollectionExtensions.cs
+++ src/ParlorPrediction.Application/DependencyInjection/ApplicationLayerServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
+using ParlorPrediction.Application.Interfaces.Dough;
+using ParlorPrediction.Application.Services.Dough;
 
 namespace ParlorPrediction.Application.DependencyInjection;
 
@@ -6,6 +8,8 @@ public static class ApplicationLayerServiceCollectionExtensions
 {
     public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
     {
+        services.AddScoped<IDoughPrepCalculationService, DoughPrepCalculationService>();
+
         return services;
     }
 }
